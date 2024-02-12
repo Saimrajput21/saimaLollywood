8a3f72f2e25a53678bc2aa6dc0f6f5361ea51554
Saim <khalidvr693@gmail.com>
2024-02-12 03:52:51 -0800

    Saim koshish nhe kertaaa

---
+++ lib/counter__application/view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'logic.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  final CounterController controller = Get.put(CounterController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo Home Page',style: TextStyle(color: Colors.purple),),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+             const Text('You have pushed the button this many times:'),
+            Obx(
+                  () => Text(
+                '${controller.count.value}',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          controller.increment();
+          print("Saim ki aqal km kam ker rhe hai...");
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+        //BIlal Saeed
+      ),
+    );
+  }
+}
